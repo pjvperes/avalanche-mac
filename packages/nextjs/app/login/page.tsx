082cdc80a7dc2e85ce41694599604441f62b740f
Gustavo Barbosa Sanchez <11802440@usp.br>
2024-02-06 14:33:49 -0300

    Middleware created and started front

---
+++ packages/nextjs/app/login/page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import SavePostButton from "~~/components/SavePostButton";
+
+const LoginPage: NextPage = () => {
+  return (
+    <>
+      <h1>Login Page</h1>
+      <SavePostButton />
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ packages/nextjs/app/myproposals/page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const MyProposals: NextPage = () => {
+  return (
+    <>
+      <div className="">
+        <h1>My Proposals</h1>
+      </div>
+    </>
+  );
+};
+
+export default MyProposals;
